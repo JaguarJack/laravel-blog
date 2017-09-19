af7c2495422bd822e36b11c75d0e7b65734c11c2
wuyanwen <82664165@qq.com>
2017-09-19 09:22:28 +0800

    edit table notice

--- database/migrations/blog/2017_09_12_085958_create_comments_notice_table.php
+++ database/migrations/blog/2017_09_12_085958_create_comments_notice_table.php
@@ -16,9 +16,9 @@ class CreateCommentsNoticeTable extends Migration
         Schema::create('notice', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->comment('被回复者ID');
-            $table->integer('from_user_name')->comment('消息来源用户昵称');
+            $table->string('from_user_name',20)->comment('消息来源用户昵称');
             $table->tinyInteger('aid')->comment('文章ID');
-            $table->tinyInteger('is_read')->comment('1:未读 2:已读');
+            $table->tinyInteger('is_read')->comment('1:未读 2:已读')->default(1);
             $table->tinyInteger('type')->comment('1:评论消息 2:关注用户发布文章消息')->default(1);
             $table->timestamps();
         });
