105f84cbaf7a6fc8d129f59df1d0633e28ff9889
wuyanwen <82664165@qq.com>
2017-09-21 20:05:50 +0800

    edit table active_email

--- database/migrations/2017_09_21_141618_table_activeEmail.php
+++ database/migrations/2017_09_21_141618_table_activeEmail.php
@@ -16,8 +16,9 @@ class TableActiveEmail extends Migration
         //
         Schema::create('active_email', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email', 50);
-            $table->string('code', 100);
+            $table->integer('user_id', 11)->comment('用户ID');
+            $table->string('email', 50)->comment('验证邮箱');
+            $table->string('code', 100)->comment('验证码');
             $table->timestamps();
         });
     }
