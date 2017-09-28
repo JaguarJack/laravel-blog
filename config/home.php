1ab74a203096cec374e1f7f512dae545a8191530
吴彦文 <1316327330@qq.com>
2017-09-28 14:48:16 +0800

    Update home.php

--- config/home.php
+++ config/home.php
@@ -17,15 +17,15 @@ return [
         'size' => 500,
     ],
     //站点网址
-    'website'    => 'https://www.njphper.com/',
+    'website'    => '',
     //后台
     'admindomain' => 'admin.blog.com',
     //前台
     'homeadomain' => 'blog.com',
     //网站基本信息
     'site'        => [
-        'title'       => 'NJphper Blog',
-        'keywords'    => '分享,社交,博客,编程,学习,问答',
-        'description' => '专注分享,分享学习成果,step by step!',
+        'title'       => '',
+        'keywords'    => '',
+        'description' => '',
     ],
 ];
