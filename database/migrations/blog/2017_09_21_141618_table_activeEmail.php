2056351a8ee6e2303ff688761ac8744ee595d648
wuyanwen <82664165@qq.com>
2017-09-30 22:58:15 +0800

    修改activeEmail表字段

--- database/migrations/blog/2017_09_21_141618_table_activeEmail.php
+++ database/migrations/blog/2017_09_21_141618_table_activeEmail.php
@@ -16,7 +16,7 @@ class TableActiveEmail extends Migration
         //
         Schema::create('active_email', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id', 11)->comment('用户ID');
+            $table->integer('user_id')->comment('用户ID');
             $table->string('email', 50)->comment('验证邮箱');
             $table->string('code', 100)->comment('验证码');
             $table->timestamps();
