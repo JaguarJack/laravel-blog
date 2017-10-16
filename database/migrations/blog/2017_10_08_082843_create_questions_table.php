4972e5469def02325f423fe41c11a8e840388104
wuyanwen <82664165@qq.com>
2017-10-16 16:01:47 +0800

    修改表结构

--- database/migrations/blog/2017_10_08_082843_create_questions_table.php
+++ database/migrations/blog/2017_10_08_082843_create_questions_table.php
@@ -15,7 +15,7 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id', 100);
+            $table->integer('user_id');
             $table->string('title', 100);
             $table->longText('subject');
             $table->integer('pv')->default(1)->comment('pv');
