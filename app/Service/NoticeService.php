20ca3e200884e57159fad740ee66bfb5803c780c
吴彦文 <1316327330@qq.com>
2017-11-22 16:07:01 +0800

    Update NoticeService.php

--- app/Service/NoticeService.php
+++ app/Service/NoticeService.php
@@ -12,18 +12,17 @@ class NoticeService
     {
         $this->notice = $notice;
     }
-    
+   
     /**
-<<<<<<< HEAD
+
      * 
      * @description:获取未读消息
      * @author wuyanwen(2017年9月18日)
      * @param@param unknown $user_id
-=======
+
      * @description:获取未读消息数量
      * @author wuyanwen(2017年9月19日)
      * @param unknown $user_id
->>>>>>> 895816a39622a9de13f40b845814919efb2232ff
      */
     public function getNotRead($user_id)
     {
